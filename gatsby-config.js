33e9f4173e5207aaee256956e91c985e6be58108
Ryan James <rejames+github@gmail.com>
2018-10-14 14:40:22 +0100

    icon link aria-labels

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -23,5 +19,9 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -16,24 +16,35 @@ export default () => {
       <nav className={style.social}>
         <a
           className={style.icon}
+          aria-label="Facebook"
           href="https://www.facebook.com/CalTheMimicMan"
         >
           <FaFacebook />
         </a>
-        <a className={style.icon} href="https://twitter.com/CalHalbert">
+        <a
+          className={style.icon}
+          aria-label="Twitter"
+          href="https://twitter.com/CalHalbert"
+        >
           <FaTwitter />
         </a>
-        <a className={style.icon} href="https://www.instagram.com/CalHalbert">
+        <a
+          className={style.icon}
+          aria-label="Instagram"
+          href="https://www.instagram.com/CalHalbert"
+        >
           <FaInstagram />
         </a>
         <a
           className={style.icon}
+          aria-label="LinkedIn"
           href="https://www.linkedin.com/in/cal-halbert-2b9a80b6/"
         >
           <FaLinkedin />
         </a>
         <a
           className={style.icon}
+          aria-label="YouTube"
           href="https://www.youtube.com/user/calhalbertcomedy"
         >
           <FaYoutube />
