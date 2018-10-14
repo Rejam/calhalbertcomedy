4a93f1ac003e72fb55dff8f5081232a0ba4c993a
Ryan James <rejames+github@gmail.com>
2018-10-14 15:18:47 +0100

    change IQ

--- src/components/imageFluid.js
+++ src/components/imageFluid.js
@@ -1,7 +1,7 @@
 export const fluidImage = graphql`
   fragment fluidImage on File {
     childImageSharp {
-      fluid(maxWidth: 800) {
+      fluid(maxWidth: 800, maxHeight: 500) {
         ...GatsbyImageSharpFluid
       }
     }
